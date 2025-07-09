dd39b4e9fef726bdfc7fe246ebb6d97c40189ecb
MPC-IT <it@multipointcom.com>
2025-07-09 23:18:28 +0000

    updated prisma schema

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,15 @@
 // lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+// Prevent multiple instances of Prisma Client in development
+declare global {
+  var prisma: PrismaClient | undefined
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma = global.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma
+}
+
+export default prisma
