878ac852db87cd77d23e64107b7caa44abcb73e5
MPC-IT <it@multipointcom.com>
2025-07-21 19:56:24 +0000

    updated users/index.ts for logic update

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,7 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import type { User } from '@prisma/client'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<User[]>) {
+type PublicUser = {
+  id: number
+  name: string
+  email: string
+  isSuspended: boolean
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<PublicUser[]>
+) {
   res.status(200).json([
     { id: 1, name: 'Admin User', email: 'admin@mpc.com', isSuspended: false },
     { id: 2, name: 'Leesa Moore', email: 'leesa@multipointcom.com', isSuspended: false },
