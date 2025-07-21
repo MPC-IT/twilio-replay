75154111e52be2130b48856bee153e5014243bf6
MPC-IT <it@multipointcom.com>
2025-07-21 19:46:14 +0000

    updated /api/users/index.ts to match schema logic

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,15 +1,9 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-type User = {
-  id: number;
-  name: string;
-  email: string;
-  isSuspended: boolean;
-};
+import type { NextApiRequest, NextApiResponse } from 'next'
+import type { User } from '@prisma/client'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<User[]>) {
   res.status(200).json([
     { id: 1, name: 'Admin User', email: 'admin@mpc.com', isSuspended: false },
     { id: 2, name: 'Leesa Moore', email: 'leesa@multipointcom.com', isSuspended: false },
-  ]);
+  ])
 }
