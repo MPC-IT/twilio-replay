f700e388981a661de5ff7dea81ea9b603a4e3d83
MPC-IT <it@multipointcom.com>
2025-07-25 19:17:24 +0000

    fix bcrypt hasPassword type error

--- lib/bcrypt.ts
+++ lib/bcrypt.ts
@@ -1,21 +1,25 @@
 import * as bcrypt from 'bcryptjs'
 
-const SALT_ROUNDS = 10
+const SALT_ROUNDS = 10;
 
 export function hashPassword(password: string): Promise<string> {
   return new Promise((resolve, reject) => {
+    if (!password) {
+      return reject(new Error('Password is required'));
+    }
+
     bcrypt.hash(password, SALT_ROUNDS, (err, hash) => {
-      if (err) reject(err)
-      else resolve(hash)
-    })
-  })
+      if (err) reject(err);
+      else resolve(hash);
+    });
+  });
 }
 
-export function verifyPassword(input: string, hashed: string): Promise<boolean> {
+export function comparePassword(password: string, hashedPassword: string): Promise<boolean> {
   return new Promise((resolve, reject) => {
-    bcrypt.compare(input, hashed, (err, isValid) => {
-      if (err) reject(err)
-      else resolve(isValid)
-    })
-  })
+    bcrypt.compare(password, hashedPassword, (err, result) => {
+      if (err) reject(err);
+      else resolve(result);
+    });
+  });
 }
